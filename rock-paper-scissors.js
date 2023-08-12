127c5d4601220a645c5cc8e4d206b064be58c8e4
Angus Warburg <amjwarburg@gmail.com>
2023-08-12 22:53:10 +0100

    Add one game functionality
    This commit adds the functionality for one game of rock, paper,
    scissors. It uses the computerSelection and playerSelection
    variables and compares them. There are messages returned for
    wins, losses and draws.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -13,5 +13,32 @@ function getComputerChoice(weapon) {
   }
 }
 
-console.log(getComputerChoice())
-console.log
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === 'rock' && computerSelection === 'paper') {
+  return 'You lose! Paper beats rock.';
+  } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    return 'You win! Rock beats scissors.'
+  } else if (playerSelection === 'rock' && computerSelection === 'rock') {
+    return 'It\'s a tie!';
+  }
+  if (playerSelection === 'paper' && computerSelection === 'rock') {
+    return 'You win! Paper beats rock.';
+  } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    return 'You lose! Scissors beats paper.';
+  } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+    return 'It\'s a tie!';
+  }
+  if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    return 'You lose! Rock beats scissors.';
+  }
+  else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    return 'You win! Scissors beats paper.';
+  } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+    return 'It\'s a tie!';
+  }
+}
+
+let playerSelection = 'paper';
+playerSelection = playerSelection.toLowerCase()
+const computerSelection = getComputerChoice()
+console.log(playRound(playerSelection, computerSelection))
